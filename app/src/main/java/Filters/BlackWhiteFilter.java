076cdd1270de8878bc74b2aff188473264e8ee19
vickysempai <39139034+vickysempai@users.noreply.github.com>
2018-11-05 20:48:56 -0600

    problema filtros

--- app/src/main/java/Filters/BlackWhiteFilter.java
+++ app/src/main/java/Filters/BlackWhiteFilter.java
@@ -2,26 +2,39 @@ package Filters;
 
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.util.Log;
 
 
 public class BlackWhiteFilter implements iFilterable {
 
     public void makeFilter(Bitmap photo){
+
         int vertical = photo.getHeight();
         int horizontal = photo.getWidth();
+        int rojo,pixel,verde,azul,suma,gris;
 
-        for (int contadorY = 0; contadorY < vertical; contadorY++){
-            for (int contadorX = 0; contadorX < horizontal; contadorX++){
-               Color color = Color.valueOf(photo.getPixel(contadorX,contadorY));
+        for (int contadorY = 0; contadorY < vertical; contadorY++)
+            for (int contadorX = 0; contadorX < horizontal; contadorX++) {
 
-               float red = color.red();
-               float blue = color.red();
-               float green = color.red();
-               int gray = (int) ((red + blue + green) /3);
+            //mi metodo
+                /*pixel = photo.getPixel(contadorX, contadorY);
+                Log.w("mitag", "obtner color");
+                pixel = photo.getPixel(contadorX, contadorY);
+                azul = pixel & 0x000000FF;
+                verde = pixel & 0x0000FF00 >> 8;
+                rojo = pixel & 0x00FF0000 >> 16;
+                gris = (azul+verde+rojo)/3;
+               // suma = ((pixel & 0xFF000000) | (rojo << 16) | (verde << 8)) | azul;
+                photo.setPixel(contadorX, contadorY, gris);*/
 
-
-               photo.setPixel(contadorX,contadorY, gray );
+                //metodo original
+               /* Color color = Color.valueOf(photo.getPixel(contadorX, contadorY));
+                float red = color.red();
+                float blue = color.blue();
+                float green = color.green();
+                int gray = (int) ((red + blue + green) / 3);
+                photo.setPixel(contadorX, contadorY, gray);*/
+                Log.w("mitag", String.valueOf(contadorX));
             }
-        }
     }
 }
--- app/src/main/java/com/picz/usuario/picz_poo/CameraActivity.java
+++ app/src/main/java/com/picz/usuario/picz_poo/CameraActivity.java
@@ -2,13 +2,14 @@ package com.picz.usuario.picz_poo;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+import android.util.Log;
 
 import Filters.BlackWhiteFilter;
 import Filters.iFilterable;
@@ -30,7 +31,13 @@ public class CameraActivity extends AppCompatActivity {
 
         Bundle parametros = this.getIntent().getExtras();
         if(parametros !=null){
+            Toast toast1 = Toast.makeText(getApplicationContext(),"aplicando filtro", Toast.LENGTH_SHORT);
+            toast1.show();
             Bitmap photo = parametros.getParcelable("Photo");
+            Log.w("mitag", "empiezo");
+            iFilterable filtro = new BlackWhiteFilter();
+            filtro.makeFilter(photo);
+            Log.w("mitag", "termine");
             imageView.setImageBitmap(photo);
         }
 
@@ -47,9 +54,6 @@ public class CameraActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
             Bitmap photo = (Bitmap)data.getExtras().get("data");
-
-            iFilterable filtro = new BlackWhiteFilter();
-            filtro.makeFilter(photo);
             imageView.setImageBitmap(photo);
         }
         else finish();
